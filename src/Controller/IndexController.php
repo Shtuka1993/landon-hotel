a9b745dccc2306a773bb16892db33526035c9647
Yuriy Shtokaylo <Shtokaylo.Yuriy@gmail.com>
2022-11-06 22:04:23 +0200

    Add annotation. Changed route to annotation

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+    /**
+    * @Route("/")
+    */
     public function home()
     {
         return new Response(
