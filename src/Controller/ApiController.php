03a36f0034b6ac4e3115636aa75a01d378921711
Yuriy Shtokaylo <Shtokaylo.Yuriy@gmail.com>
2022-11-09 16:09:24 +0200

    Add API controller

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,23 @@
+<?php
+
+    namespace App\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use App\Entity\Hotel;
+
+    class ApiController extends AbstractController {
+        /**
+         * @Route("/api/rooms")
+         */
+        public function home() {
+            $hotels = $this->getDoctrine()
+                ->getRepository(Hotel::class)
+                ->findAll();
+            
+                return $this->json([
+                    'hotels' => $hotels
+                ]);
+        }
+    }
